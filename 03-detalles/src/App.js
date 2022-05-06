df5db5c9ff55c55b7f5d302574725c8c7c658af2
Uriel.Florez <urielflorez29@gmail.com>
2022-05-06 18:49:11 -0500

    actualizacion de 03

--- 03-detalles/src/App.js
+++ 03-detalles/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Calculadora from "./components/Calculadora";
 
 const App = () => {
-    return ( <div className = "container text-center" >
-        <h1> Calculator - PWA </h1> 
-        <hr />
-        <Calculadora />
+    return (
+        <div className="container text-center" >
+            <h1> Calculator - PWA </h1> 
+            <hr />
+            <Calculadora />
         </div>
     );
 };
