5105920cfe15bebf44dd76ace6ac252efeb411e6
Uriel.Florez <urielflorez29@gmail.com>
2022-05-06 20:54:29 -0500

    Tema de hooks 04

---
+++ 04-hooks/src/App.js
@@ -0,0 +1,29 @@
+// import Calculadora from "./components/Calculadora";
+// import Effect from "./components/Effect";
+// import State from "./components/State";
+// import LayoutEffect from "./components/LayoutEffect";
+// import Ref from "./components/Ref";
+// import Memorize from "./components/memos/Memorize";
+
+import Custom from "./components/Custom";
+import Effect from "./components/Effect";
+import Memorize from "./components/memos/Memorize";
+
+const App = () => {
+  return (
+    <div className="container text-center">
+      <Effect />
+    </div>
+    //<Memorize />
+    // <State />
+
+    // <div className="container text-center">
+    //   <h1>Calculadora - PWA</h1>
+    //   <hr />
+
+    //   <Calculadora />
+    // </div>
+  );
+};
+
+export default App;
